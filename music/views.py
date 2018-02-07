6ff8497f500692babe9dd85a9c0ac70a8dd1ece3
CycloneBoy <xuanfeng1992@gmail.com>
2018-02-08 01:16:29 +0800

    finish 24

--- music/views.py
+++ music/views.py
@@ -1,9 +1,26 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 
 def index(request):
-    return HttpResponse("<h1>This will be a list of all albums</h1>")
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    album = get_object_or_404(Album,pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        select_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message':"You did not select a valid song!",
+        })
+    else:
+        select_song.is_favorite = True
+        select_song.save()
+        return render(request, 'music/detail.html', {'album': album})
