b065901ac4ff309d742a80a5e776484ef944a752
CycloneBoy <xuanfeng1992@gmail.com>
2018-02-08 10:23:15 +0800

    add app blog some code

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # /music/
+    # /blog/
     url(r'^$', views.blog_title, name='blog_title'),
+
+    # /blog/<article_id>/
+    url(r'^(?P<article_id>[0-9]+)/$', views.blog_articles, name='blog_detail'),
 
 ]
